e167bb05a72b6d2fca754dd32a046680c29e0239
Chris <ctrennells2005@gmail.com>
2023-01-12 17:08:06 -0500

    Sixth Commit

--- src/ItemsDisplay.js
+++ src/ItemsDisplay.js
@@ -1,16 +1,36 @@
 function ItemsDisplay(props) {
   const showItem = (item) => {
     return (
-      <div>
-        <p>Id: {item.id}</p>
-        <p>Name: {item.name}</p>
-        <p>Price: {item.price}</p>
-        <p>Type: {item.type}</p>
-        <p>Brand: {item.brand}</p>
-      </div>
+      <tr>
+        <th scope="row">{item.id}</th>
+        <td>{item.name}</td>
+        <td>{item.price}</td>
+        <td>{item.type}</td>
+        <td>{item.brand}</td>
+      </tr>
     );
   };
-  return <div>{props.items.map(showItem)}</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <h2>Items</h2>
+      </div>
+      <div className="row">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="colo">Id</th>
+              <th scope="colo">Name</th>
+              <th scope="colo">Price</th>
+              <th scope="colo">Type</th>
+              <th scope="colo">Brand</th>
+            </tr>
+          </thead>
+          <tbody>{props.items.map(showItem)}</tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default ItemsDisplay;
